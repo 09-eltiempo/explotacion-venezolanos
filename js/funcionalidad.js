fe9c89dc1d1151cc770a83d44035385203c7b41b
carlosbustos <carlosbustosc09@gmail.com>
2025-04-24 10:20:50 -0500

    Inicio de proyecto original de 30 expertos fondo azul

---
+++ js/funcionalidad.js
@@ -0,0 +1,230 @@
+$(document).ready(function(){
+
+
+
+const jsonURL = 'https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/data/data.json?80';
+let contenido = document.querySelector(".seccion");
+
+/*------descripcion inicial-----------*/
+let descripcionInicial = document.querySelector(".descripcion").innerHTML = "TREINTA EXPERTOS DE DIFERENTES ÁREAS COMPARTIERON CON EL TIEMPO SU VISIÓN DE LO QUE ESTAMOS VIVIENDO Y LO QUE VIENE DESPUÉS DEL CORONAVIRUS.";
+
+/*-------------contenidos-------------*/
+let contenido_principal = document.querySelector(".contenido_principal");
+let ilustracion_movile = document.querySelector(".ilustracion_movile");
+let body                = document.querySelector("body");
+let contenido_articulo  = document.querySelector(".contenido_articulo");
+/*-------------Fin contenidos-------------*/
+
+/*------------articulos-----------------*/
+let imagenInicio  = document.querySelector("#imagen_autor_desktop");
+let imagenInicio2 = document.querySelector("#imagen_autor_mobile");
+let titulo        = document.querySelector(".titulo_articulo");
+let autor         = document.querySelector(".autor");
+let descripcion   = document.querySelector(".descripcion2");
+let articulo      = document.querySelector(".base-articulo-completo");
+/*------------Fin articulos-----------------*/
+
+let cerrar        = document.querySelector("#cerrar-desktop");//boton cerrar
+let divisor       = document.querySelector(".base_divisor_desktop");
+
+ilustracion_movile.style.marginTop = '-80px';
+
+/*---------redes----*/
+let redesSociales = document.querySelector(".centrar_redes");
+redesSociales.innerHTML = `
+
+					<div class="contenido_redes_sociales">
+						<a href="https://www.facebook.com/eltiempo/" target="blank">
+							<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/icono-facebook.svg" alt="" class="icono_redes quitar_espacio" data-aos="fade-up-left" data-aos-duration="700">
+						</a>
+						<a href="https://twitter.com/ELTIEMPO" target="blank">
+							<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/icono-twiter.svg"   alt="" class="icono_redes" data-aos="fade-up-left" data-aos-duration="1000">
+						</a>
+						<a href="#"><img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/icono-whatsapp.svg" alt="" class="icono_redes" data-aos="fade-up-left" data-aos-duration="1300"></a>
+						<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/icono-puntos.svg"   alt="" class="icono_redes tres_puntos" data-aos="fade-up-left" data-aos-duration="1500">
+
+						<div class="redes_desplegable">
+							<a href="https://www.facebook.com/eltiempo/" target="blank">
+								<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/icono-facebook.svg" alt="" class="icono_redes">
+							</a>
+							<a href="https://twitter.com/ELTIEMPO" target="blank">
+								<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/icono-twiter.svg"   alt="" class="icono_redes">
+							</a>
+						</div>
+					</div>`
+					
+					let redes  = document.querySelector(".tres_puntos");
+					
+					var comprobar = 1;
+					redes.addEventListener("click", function(){
+						if(comprobar == 1){
+							document.querySelector(".redes_desplegable").style.display = "table";
+							comprobar =0;
+						}else{
+							 comprobar =1;
+							 document.querySelector(".redes_desplegable").style.display = "none";
+						}
+					})
+
+					setTimeout(function(){
+						$(".icono_redes").removeAttr('data-aos');
+					}, 3000)
+					
+/*---------fin redes----*/
+/*---llamado de data----*/
+
+    $.getJSON(jsonURL, function(data){
+	    myData = data;
+	    //console.log(myData);
+    	let hexagono;
+		for(i=0; i<myData.autores.length; i++){
+				console.log(myData.autores[i].nombre);
+
+		 		hexagono =  
+
+					`
+					<div class="hexagono1" id= "autor${i}" data-aos="fade-up-left" data-aos-duration="1500">
+						<figure>
+							<p class="titular_hexagono">${myData.autores[i].titulo}</p>
+							<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/hexagono-titular.svg" class="hexagonos frontal   hexagono-resposive-frontal">
+							<div class="dejar-estatico trasera">
+								<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/hexagono-autor.svg" class="hexagonos frontal hexagono-resposive-atras">
+								<div class="contenido_informacion">
+									<center><img src="${myData.autores[i].foto}" class="hexagonos frontal imagen-autor"></center>
+									<h2 class="Nombre-Autor">${myData.autores[i].nombre}</h2>
+									<p class="descripcion_articulo">${myData.autores[i].descripcion}</p>
+									<div id="${i}" class="verMas">
+										<hr class="linea-divisora">
+										<a><p class="leer_mas">LEER <br> <span class="signo">+</span></p></a>
+									</div>
+								</div>
+							</div>
+						</figure>
+					</div>
+					`
+			contenido.innerHTML += hexagono;
+
+			if($(window).width() < '600') {
+			
+				var  frontal = $(".hexagono-resposive-frontal");
+				var  atras   = $(".hexagono-resposive-atras");
+				frontal.attr('src', 'https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/frontal-responsive.svg');
+				atras.attr('src', 'https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/back-responsive.svg');										  		
+			}
+			
+			$(".verMas").click(function(e) {
+				let id = $(this).attr('id');//console.log(id)
+				let elemento = document.getElementById(id);//cojer el div
+				let padre = elemento.parentElement.parentElement.parentElement;//cojer el padre
+				//var elemento = $(this).addClass("carlos");
+				padre.classList.add('dejar-estatico');
+				console.log(padre);
+
+				ilustracion_movile.style.marginTop = '0px';
+				contenido_principal.style.display = "none";
+				body.classList.add('fondo_body');
+				contenido_articulo.style.display  = "block";
+
+				var elmnt = document.querySelector(".base-articulo-completo");
+					var x = elmnt.scrollLeft;
+					var y = elmnt.scrollTop = 0;
+					console.log(y);
+				
+
+				if($(window).width() < '600') {
+  					
+				  	setTimeout(function(){
+						divisor.style.opacity = "1";
+					}, 1200)
+				}
+
+				for(let i=0; i< myData.autores.length; i++){
+
+					if(id == myData.autores[i].id){
+						//console.log(id);
+						//console.log(padre);
+						imagenInicio.src       =  myData.autores[i].imagenAutor;
+						imagenInicio2.src      =  myData.autores[i].imagenAutor;
+						titulo.innerHTML       =  myData.autores[i].titulo;
+						autor.innerHTML        =  myData.autores[i].nombre;
+						descripcion.innerHTML  =  myData.autores[i].descripcion;
+						articulo.innerHTML     =  ` 
+
+						<div class="base_textos esconder_desktop" id="titulo">
+							<h1 class="titulo_articulo">${myData.autores[i].titulo}</h1>
+							<h2 class="autor ancho_caja">${myData.autores[i].nombre}</h2>
+							<p class="descripcion2 ancho_caja">${myData.autores[i].descripcion}</p>
+						</div>
+
+						${myData.autores[i].articulo}
+
+													<div class="centrar_redes bajar_redes_z">
+														<hr class="linea_divisora_red">
+														<div class="contenido_redes_sociales redes_mobile">
+															<a href="https://www.facebook.com/eltiempo/" target="blank">
+																<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/icono-facebook.svg" alt="" class="icono_redes redes_movile margen-f tamano-icon" >
+															</a>
+															<a href="https://twitter.com/ELTIEMPO" target="blank">
+																<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/icono-twiter.svg"   alt="" class="icono_redes redes_movile">
+															</a>
+															<a href="#"><img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/icono-whatsapp.svg" alt="" class="icono_redes redes_movile"></a>
+															
+															<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/icono-puntos.svg"   alt="" class="icono_redes redes_movile tamano-icon puntos2">
+															<div class="red_adicional">
+																<a href="https://twitter.com/ELTIEMPO" target="blank">
+																	<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/icono-twiter.svg"   alt=""  class="icono_redes redes_movile  left_movile">
+																</a>
+
+																<a href="https://twitter.com/ELTIEMPO" target="blank">
+																	<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/icono-facebook.svg"   alt="" class="icono_redes redes_movile  left_movile">
+																</a>
+															</div>
+															<img src="https://www.eltiempo.com/infografias/2016/Explotacion_laboral_venezolanos/img/flecha_atras.png"   class="icono_redes flecha_atras redes_movile">
+														</div>
+													</div>	`;
+
+						let puntos = document.querySelector(".puntos2");
+						puntos.addEventListener("click", function(){
+							document.querySelector(".red_adicional").style.display = "block";
+							document.querySelector(".flecha_atras").style.display  = "block";
+							puntos.style.display = "none";
+						});
+
+						let ocultar = document.querySelector(".flecha_atras");
+						ocultar.addEventListener("click", function(){
+							puntos.style.display = "block";
+							document.querySelector(".red_adicional").style.display = "none";
+							document.querySelector(".flecha_atras").style.display  = "none";
+						});
+
+					}
+					
+				}/*---fin for--*/
+					
+			});/*--fin click-*/
+			
+
+		}
+
+	})
+
+	cerrar.addEventListener("click", function(){
+		contenido_principal.style.display = "block";
+		body.classList.remove('fondo_body');
+		contenido_articulo.style.display  = "none";
+		ilustracion_movile.style.marginTop = '-250px';
+		if($(window).width() < '600') {
+			divisor.style.opacity = "0";
+		}
+	});
+
+
+});
+
+
+
+
+
+
+
+  	
